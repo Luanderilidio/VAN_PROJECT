5343687d15a7eff3cc2e4eeef28a5eae704b2880
luander <luander.arruda@unemat.br>
2022-09-05 07:56:11 -0400

    [feat]: implementa formulario de endereco

--- van_project/src/components/FormDelivery/index.js
+++ van_project/src/components/FormDelivery/index.js
@@ -2,48 +2,102 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Alert,
-  Autocomplete,
   Button,
   FormControl,
   FormControlLabel,
   FormLabel,
   Grid,
+  InputLabel,
+  MenuItem,
   Radio,
   RadioGroup,
+  Select,
   Snackbar,
   Stack,
   TextField,
+  Typography,
+  IconButton,
+  Box,
+  Modal,
 } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { MdAdd } from "react-icons/md";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { ptBR } from "date-fns/locale";
+import styleModal from "./styles";
 import api from "../../services/api";
 
-const FormDelivery = function () {
+const FormDelivery = function ({ id, testNewRequest }) {
   const [date, setDate] = useState(new Date());
   const [valueDate, setValueDate] = useState(new Date());
-  const [empresa, setEmpresa] = useState("");
   const [responsavel, setResponsavel] = useState("");
-  const [perecivel, setPerecivel] = useState(false);
+  const [descricao, setDescricao] = useState("");
   const [qtd, setQtd] = useState(0);
-  const [valueRadio, setValueRadio] = useState(true);
-  const [allNames, setAllNames] = useState([]);
+  const [valueRadio, setValueRadio] = useState("");
+  const [nameEmpresa, setNameEmpresa] = useState("");
+  const [allEmpresa, setAllEmpresa] = useState([]);
+  const [empresa, setEmpresa] = useState("");
   const [openSnack, setOpenSnack] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [zipCode, setZipCode] = useState();
+  const [city, setCity] = useState();
+  const [district, setDistrict] = useState();
+  const [street, setStreet] = useState();
+  const [number, setNumber] = useState();
+  const [complement, setComplement] = useState();
+
+  const handleModal = () => setOpenModal(!openModal);
 
   const navigate = useNavigate();
+
   const handleSnack = () => {
     setOpenSnack(!openSnack);
   };
 
+  const handleAllEmpresa = async () => {
+    try {
+      const response = await api.get("/empresa");
+      setAllEmpresa(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setEmpresa(event.target.value);
+    handleAllEmpresa();
+  };
+
+  const dataEmpresa = {
+    name: nameEmpresa,
+  };
+
   const handleEmpresa = async () => {
     try {
-      const response = await api.get("http://localhost:8000/empresa");
-      setEmpresa(response.data);
-      const names = [];
-      response.data.forEach((item, index) => {
-        names.push({ label: item.name, id: item.id });
-      });
-      setAllNames(names);
+      const response = await api.post("/empresa", dataEmpresa);
+      console.log(response.data);
+      handleSnack();
+      handleAllEmpresa();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("requisicao concluida!");
+    }
+  };
+
+  const handleRequestId = async () => {
+    try {
+      const response = await api.get(`/entregas/${id}`);
+      setResponsavel(response.data.response);
+      setDescricao(response.data.description);
+      setQtd(response.data.quantidade);
+      setEmpresa(response.data.uuid_empresa);
+      setValueDate(response.data.date1);
+      setValueRadio(response.data.perecivel ? 1 : 2);
+      setCity(response.data.cidade);
+      setDistrict(response.data.bairro);
+      setStreet(response.data.rua);
+      setNumber(response.data.numero);
     } catch (error) {
       console.error(error);
     }
@@ -54,80 +108,111 @@ const FormDelivery = function () {
     perecivel: valueRadio === "1",
     quantidade: qtd,
     response: responsavel,
+    description: descricao,
     date1: valueDate,
     status: false,
-  };
-
-  const limpaCampus = () => {
-    setDate("");
-    setEmpresa("");
-    setResponsavel("");
-    setQtd("");
+    cidade: city,
+    bairro: district,
+    rua: street,
+    numero: number,
   };
 
   const handlePedido = async () => {
     try {
       const response = await api.post("/entregas/", data);
       handleSnack();
-      limpaCampus();
     } catch (error) {
       console.error(error);
     }
-    // const inputs = Array.from(document.querySelectorAll("input"));
-    // inputs.map((input) => {
-    //   input.value = "";
-    // });
-    // Array.from(document.querySelectorAll("input")).forEach(
-    //   (input) => (input.value = "")
-    // );
   };
 
+  const handlePutPedido = async () => {
+    try {
+      const response = await api.put(`/entregas/${id}`, data);
+      handleSnack();
+      testNewRequest();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(id);
   useEffect(() => {
-    handleEmpresa();
-    const inputs = Array.from(document.querySelectorAll("input"));
-    // inputs.map((input) => (input.value = ""));
-    // console.log(inputs);
+    handleAllEmpresa();
+    if (id) {
+      handleRequestId();
+    }
   }, []);
+
+  const responsive = id ? 12 : 11;
 
   return (
     <Grid container spacing={2}>
-      <Grid item mobile={12}>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={allNames}
-          isOptionEqualToValue={(option) => {
-            setEmpresa(option.id);
-            return option.label;
+      <Grid item mobile={responsive} notebook={responsive} desktop={responsive}>
+        <FormControl fullWidth>
+          <InputLabel>Supermercado</InputLabel>
+          <Select value={empresa} label="Supermercado" onChange={handleChange}>
+            {allEmpresa.map((x) => (
+              <MenuItem key={x.id} value={x.id}>
+                {x.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      {id ? (
+        ""
+      ) : (
+        <Grid
+          item
+          mobile={1}
+          notebook={1}
+          desktop={1}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
-          renderInput={(params) => (
-            <TextField fullWidth {...params} label="Supermercado" />
-          )}
+        >
+          <IconButton onClick={handleModal}>
+            <MdAdd />
+          </IconButton>
+        </Grid>
+      )}
+      <Grid item mobile={12}>
+        <TextField
+          fullWidth
+          value={responsavel}
+          label="Cliente"
+          onChange={(event) => setResponsavel(event.target.value)}
         />
       </Grid>
       <Grid item mobile={12}>
         <TextField
+          multiline
           fullWidth
-          label="Cliente"
-          onBlur={(event) => setResponsavel(event.target.value)}
+          value={descricao}
+          label="Descrição"
+          onChange={(event) => setDescricao(event.target.value)}
         />
       </Grid>
-      <Grid item mobile={12}>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
         <TextField
           fullWidth
           type="number"
+          value={qtd}
           label="Quantidade"
-          onBlur={(event) => setQtd(parseInt(event.target.value, 10))}
+          onChange={(event) => setQtd(parseInt(event.target.value, 10))}
         />
       </Grid>
-      <Grid item mobile={12}>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
         <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
           <DatePicker
             label="Data Limite"
             value={valueDate}
             onChange={(newValue) => {
               setValueDate(newValue);
-              setDate(newValue.toLocaleDateString("pt-BR"));
+              // setDate(newValue.toLocaleDateString("pt-BR"));
             }}
             renderInput={(params) => <TextField fullWidth {...params} />}
           />
@@ -136,40 +221,105 @@ const FormDelivery = function () {
       <Grid item mobile={12}>
         <FormControl fullWidth>
           <FormLabel id="demo-row-radio-buttons-group-label">
-            Produtos Pereciveis?
+            Produtos Perecíveis?
           </FormLabel>
           <RadioGroup
             row
+            checked={valueRadio}
             value={valueRadio}
             onChange={(event) => {
               setValueRadio(event.target.value);
+              console.log(valueRadio);
             }}
           >
             <FormControlLabel value={1} control={<Radio />} label="Sim" />
-            <FormControlLabel value={2} control={<Radio />} label="Nao" />
+            <FormControlLabel value={2} control={<Radio />} label="Não" />
           </RadioGroup>
         </FormControl>
       </Grid>
-
       <Grid item mobile={12}>
-        <Stack justifyContent="space-between" direction="row" fullWidth>
-          <Button
-            onClick={() => navigate("/delivery")}
-            color="info"
-            variant="contained"
-          >
-            Visualizar
-          </Button>
-          <Button
-            onClick={() => {
-              handlePedido();
-              limpaCampus();
-            }}
-            color="success"
-            variant="contained"
-          >
-            Cadastrar
-          </Button>
+        <Typography gutterBottom variant="h5">
+          Endereço
+        </Typography>
+      </Grid>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
+        <TextField
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+          fullWidth
+          label="Cidade"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
+        <TextField
+          fullWidth
+          value={district}
+          onChange={(event) => setDistrict(event.target.value)}
+          label="Bairro"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
+        <TextField
+          fullWidth
+          value={street}
+          onChange={(event) => setStreet(event.target.value)}
+          label="Rua"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item mobile={12} tablet={6} laptop={6} notebook={6} desktop={6}>
+        <TextField
+          value={number}
+          onChange={(event) => setNumber(event.target.value)}
+          fullWidth
+          label="N°"
+          variant="outlined"
+        />
+      </Grid>
+
+      <Grid item mobile={12} tablet={12} laptop={12} notebook={12} desktop={12}>
+        <Stack
+          justifyContent="space-between"
+          direction="row"
+          sx={{ width: "100%" }}
+        >
+          {id ? (
+            <>
+              <Box />
+              <Button
+                onClick={() => {
+                  handlePutPedido();
+                }}
+                color="success"
+                variant="contained"
+              >
+                Editar
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={() => {
+                  navigate("/delivery");
+                }}
+                color="info"
+                variant="contained"
+              >
+                Visualizar
+              </Button>
+              <Button
+                onClick={() => {
+                  handlePedido();
+                }}
+                color="success"
+                variant="contained"
+              >
+                Cadastrar
+              </Button>
+            </>
+          )}
         </Stack>
       </Grid>
       <Snackbar
@@ -179,9 +329,47 @@ const FormDelivery = function () {
         onClose={handleSnack}
       >
         <Alert onClose={handleSnack} severity="success" sx={{ width: "100%" }}>
-          Pedido Cadastrado com Sucesso!
+          Cadastrado com Sucesso!
         </Alert>
       </Snackbar>
+
+      <Modal open={openModal} onClose={handleModal}>
+        <Box sx={styleModal}>
+          <Typography gutterBottom variant="h6" component="h2">
+            Cadastrar novo Supermercado
+          </Typography>
+          <TextField
+            fullWidth
+            label="Nome"
+            onChange={(event) => setNameEmpresa(event.target.value)}
+          />
+          <Stack
+            witdh="100%"
+            mt={2}
+            justifyContent="space-between"
+            direction="row"
+          >
+            <Button
+              onClick={() => {
+                handleModal();
+              }}
+              color="info"
+              variant="contained"
+            >
+              Fechar
+            </Button>
+            <Button
+              onClick={() => {
+                handleEmpresa();
+              }}
+              color="success"
+              variant="contained"
+            >
+              Casdastrar
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
     </Grid>
   );
 };
---
+++ van_project/src/components/FormDelivery/styles.js
@@ -0,0 +1,12 @@
+const styleModal = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 350,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 4,
+  p: 4,
+};
+export default styleModal;
