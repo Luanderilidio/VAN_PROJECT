6d514c8bfbda1e276464a05a21489e483454b157
luander <luander.arruda@unemat.br>
2022-09-08 14:20:17 -0400

    [style]: adiciona animacao fadeIn ao card

--- van_project/src/pages/Cadastrar/index.js
+++ van_project/src/pages/Cadastrar/index.js
@@ -1,12 +1,17 @@
 import { Card, Container, Typography } from "@mui/material";
 import FormDelivery from "../../components/FormDelivery";
 import Page from "../../components/page";
+import "animate.css";
 
 const Cadastrar = function () {
   return (
     <Page>
       <Container maxWidth="desktop">
-        <Card elevation={3} sx={{ padding: 3, borderRadius: 4 }}>
+        <Card
+          className="animate__animated animate__fadeIn"
+          elevation={3}
+          sx={{ padding: 3, borderRadius: 4 }}
+        >
           <Typography gutterBottom variant="h5">
             Cadastrar
           </Typography>
